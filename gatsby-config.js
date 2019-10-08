cb57b8e8af6dc2145f9ff99e292b22988abadc44
mbeche <mb.johnsonbece@gmail.com>
2019-10-08 18:54:09 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `Where I put all my thoughts and stuff.`,
+    author: `@micLeners`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.API_KEY,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
